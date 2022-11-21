d17f0f0d14266b9a33a6e3e52442cf6bd2dd0563
Mahmud <jmbengal.mbstu@gmail.com>
2022-11-21 14:42:13 +0600

    continent fetched using graphQL

--- root.js
+++ root.js
@@ -1,1 +1,41 @@
-console.log('=================> connected')
+// console.log('=================> connected')
+const continentSelect = document.getElementById("select-continent");
+// console.log('====================> ok', continentSelect)
+
+// fetch all data from web app link 
+function queryFetch(query, variables) {
+    return fetch('https://countries.trevorblades.com/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    }).then(res => res.json())
+}
+
+queryFetch(`
+  query {
+    continents {
+      name
+      code
+    }
+  }
+`).then(data => {
+    data.data.continents.forEach(continent => {
+        const option = document.createElement('option')
+        option.value = continent.code
+        option.innerText = continent.name
+        continentSelect.append(option)
+        //   console.log(option)
+    })
+    // console.log(data.data)
+})
+
+const selectedData = () => {
+    // console.log('========> Selected')
+    var selectedContinent = document.getElementById('select-continent');
+    var value = selectedContinent.options[selectedContinent.selectedIndex].text;
+    console.log("The selected value=" + value);
+} 
+
